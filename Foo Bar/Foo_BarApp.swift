de0088d43ed2ed18349dcab9f678c863f3dc5666
Nerto Kageh <kagehnerto@yahoo.com>
2022-08-25 14:56:48 -0500

    Initial Commit

---
+++ Foo Bar/Foo_BarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Foo_BarApp.swift
+//  Foo Bar
+//
+//  Created by Induran Aderinto  on 8/25/22.
+//
+
+import SwiftUI
+
+@main
+struct Foo_BarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
