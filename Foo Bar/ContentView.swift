af8759141ae7feb9b9e5f1f6c043a5f19ad73c6e
Nerto Kageh <kagehnerto@yahoo.com>
2022-08-26 09:48:26 -0500

    Added On tap gesture and on long press gesture.

--- Foo Bar/ContentView.swift
+++ Foo Bar/ContentView.swift
@@ -8,8 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var counter = 0
+
     var body: some View {
-        Text("Hello, world!")
+        Text("\(counter)")
+                  .font(.system(size: 128))
+                  .multilineTextAlignment(.center)
+                  .preferredColorScheme(.dark)
+                  .onTapGesture {
+                                  counter += 1
+                              }
+                  .onLongPressGesture {
+                                 counter = 0
+                             }
+
+
+
             .padding()
     }
 }
